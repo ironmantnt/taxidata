c5e6ee6e8f8d5887389123b532033eec912fb71b
bolan2014 <1139217488@qq.com>
2016-08-02 11:15:42 +0800

    update

--- src/util/myTime.py
+++ src/util/myTime.py
@@ -3,6 +3,8 @@ Transfer time stamp to neural network input
 	@bolan on 13rd, June
 '''
 
+from datetime import datetime as dt
+
 def time_transfer(time):
 	hour, minute = time[:2], time[2:4]
 	minute = round(float(minute) / 60, 1)
@@ -18,10 +20,16 @@ def time_transfer(time):
 		minute = 0.8
 	return int(hour) + minute
 
+def time_duration(t1, t2):
+	print t1, t2
+	a, b = dt.strptime(t1, '%H%M%S'), dt.strptime(t2, '%H%M%S')
+	return repr((b - a).seconds)
+
 if __name__ == '__main__':
 	a = '010203'
-	b = '093012'
+	b = '010312'
 	c = '134520'
 
-	time = [a,b,c]
-	print map(time_transfer, time)
+	#time = [a,b,c]
+	#print map(time_transfer, time)
+	print time_duration(a, b)
